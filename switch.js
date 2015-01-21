38262fe65ba263544b148709adef03d834bd9ac1
James McAulay <hello@jamesmcaulay.co.uk>
2015-01-21 00:55:10 +0000

    Code added

---
+++ switch.js
@@ -0,0 +1,136 @@
+/* INSERT YOUR EMAIL HERE */ 
+/* var email = */ 
+var domain = email.replace(/.*@/, "");
+switch (domain) {
+	case "all-souls.ox.ac.uk":
+		console.log("All Souls");
+		break;
+	case "balliol.ox.ac.uk":
+		console.log("Balliol");
+		break;
+	case "bfriars.ox.ac.uk":
+		console.log("Blackfriars");
+		break;
+	case "bnc.ox.ac.uk":
+		console.log("Brasenose");
+		break;
+	case "campion.ox.ac.uk":
+		console.log("Campion Hall");
+		break;
+	case "chch.ox.ac.uk":
+		console.log("Christ Church");
+		break;
+	case "ccc.ox.ac.uk":
+		console.log("Corpus Christi");
+		break;
+	case "exeter.ox.ac.uk":
+		console.log("Exeter");
+		break;
+	case "gtc.ox.ac.uk":
+		console.log("Green Templeton");
+		break;
+	case "hmc.ox.ac.uk":
+		console.log("Harris Manchester");
+		break;
+	case "hertford.ox.ac.uk":
+		console.log("Hertford");
+		break;
+	case "jesus.ox.ac.uk":
+		console.log("Jesus");
+		break;
+	case "keble.ox.ac.uk":
+		console.log("Keble");
+		break;
+	case "kellogg.ox.ac.uk":
+		console.log("Kellogg");
+		break;
+	case "lmh.ox.ac.uk":
+		console.log("Lady Margaret Hall");
+		break;
+	case "linacre.ox.ac.uk":
+		console.log("Linacre");
+		break;
+	case "lincoln.ox.ac.uk":
+		console.log("Lincoln");
+		break;
+	case "magd.ox.ac.uk":
+		console.log("Magdalen");
+		break;
+	case "mansfield.ox.ac.uk":
+		console.log("Mansfield");
+		break;
+	case "merton.ox.ac.uk":
+		console.log("Merton");
+		break;
+	case "new.ox.ac.uk":
+		console.log("New");
+		break;
+	case "nuffield.ox.ac.uk":
+		console.log("Nuffield");
+		break;
+	case "oriel.ox.ac.uk":
+		console.log("Oriel");
+		break;
+	case "pmb.ox.ac.uk":
+		console.log("Pembroke");
+		break;
+	case "regents.ox.ac.uk":
+		console.log("Regent's Park");
+		break;
+	case "some.ox.ac.uk":
+		console.log("Somerville");
+		break;
+	case "st-annes.ox.ac.uk":
+		$('#College').val("St Anne's");
+		break;
+	case "sant.ox.ac.uk":
+		console.log("St Antony's");
+		break;
+	case "stb.ox.ac.uk":
+		console.log("St Benet's Hall");
+		break;
+	case "stcatz.ox.ac.uk":
+		console.log("St Catherine's");
+		break;
+	case "stx.ox.ac.uk":
+		console.log("St Cross");
+		break;
+	case "seh.ox.ac.uk":
+		console.log("St Edmund Hall");
+		break;
+	case "st-hildas.ox.ac.uk":
+		console.log("St Hilda's");
+		break;
+	case "st-hughes.ox.ac.uk":
+		console.log("St Hugh's");
+		break;
+	case "sjc.ox.ac.uk":
+		console.log("St John's");
+		break;
+	case "spc.ox.ac.uk":
+		console.log("St Peter's");
+		break;
+	case "ssho.ox.ac.uk":
+		console.log("St Stephen's");
+		break;
+	case "queens.ox.ac.uk":
+		console.log("Queen's");
+		break;
+	case "trinity.ox.ac.uk":
+		console.log("Trinity");
+		break;
+	case "univ.ox.ac.uk":
+		console.log("University");
+		break;
+	case "wadh.ox.ac.uk":
+		console.log("Wadham");
+		break;
+	case "wolfson.ox.ac.uk":
+		console.log("Wolfson");
+		break;
+	case "worc.ox.ac.uk":
+		console.log("Worcester");
+		break;
+	case "wycliffe.ox.ac.uk":
+		$('#wycliffe').val("Wycliffe Hall");
+		break;
